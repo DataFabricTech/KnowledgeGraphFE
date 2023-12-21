4d561e945e0a799be7b8547bc95e4c1f60d096e1
YunSeong Park <qkrdbstjd95@naver.com>
2023-12-21 11:17:05 +0900

    [New Feature/ Demo App] 페이지 구현

---
+++ demo-app/src/app/pageInnerContents.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Canvas from "@/components/canvas/canvas";
+import DoInfoSide from "@/components/doInfoSide/doInfoSide";
+import Drawer from "@/components/drawer/drawer";
+import Header from "@/components/header/header";
+import Tab from "@/components/tab/tab";
+import { useRootStore } from "@/store/storeContext";
+import { observer } from "mobx-react-lite";
+
+const Inner = observer(() => {
+  const root = useRootStore();
+  console.log(process.env);
+  return (
+    <>
+      <Tab
+        tabList={[{ name: "test", id: "test" }]}
+        selectedId="test"
+        onSelect={() => {}}
+      />
+      <Header />
+
+      <Drawer
+        open={Boolean(root.targetNode)}
+        onClose={() => {
+          root.setTargetNode(null);
+        }}
+      >
+        <DoInfoSide />
+      </Drawer>
+      <Canvas />
+    </>
+  );
+});
+
+export default Inner;
