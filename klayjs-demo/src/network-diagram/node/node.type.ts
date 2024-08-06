0925784aa38b3a1f30f0f78582b07cfd4e7bfb35
ys_park <yunseong_park@tmax.co.kr>
2024-08-06 11:05:00 +0900

    [type] node 타입 정의

---
+++ klayjs-demo/src/network-diagram/node/node.type.ts
@@ -0,0 +1,44 @@
+import { RequiredKeys } from "@/util/type.util";
+import { ElkNode } from "elkjs";
+import { BBox } from "rbush";
+
+export type NetworkDiagramNodeLayout = RequiredKeys<
+  ElkNode,
+  "width" | "height" | "x" | "y"
+>;
+
+export type NetworkDiagramNodeStyle = {
+  fontSize: number;
+  fontColor: string;
+  fontWeight: number;
+  backgroundColor: string;
+  outlineColor: string;
+  outlineWeight: number;
+  outlineDiff: number;
+};
+
+export interface NetworkDiagramNode<
+  Style extends NetworkDiagramNodeStyle = NetworkDiagramNodeStyle
+> extends NetworkDiagramNodeLayout,
+    BBox {
+  setStyle(style: Partial<Style>): void;
+  defaultStyle(): void;
+  activeStyle(): void;
+  focusStyle(): void;
+
+  renderOffscreen(scale: number): void;
+
+  draw(
+    ctx: CanvasRenderingContext2D,
+    scale: number,
+    offsetX: number,
+    offsetY: number
+  ): void;
+
+  // drawEventLayer(
+  //   ctx: CanvasRenderingContext2D,
+  //   scale: number,
+  //   offsetX: number,
+  //   offsetY: number
+  // ): void;
+}
