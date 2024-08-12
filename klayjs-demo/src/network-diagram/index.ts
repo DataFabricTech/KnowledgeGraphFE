872a17e3a31aff5e2fb4e26991163320b37a86cb
ys_park <yunseong_park@tmax.co.kr>
2024-08-12 16:09:59 +0900

    [New Feature] Network Diagram 구성

--- klayjs-demo/src/network-diagram/index.ts
+++ klayjs-demo/src/network-diagram/index.ts
@@ -1,1 +1,58 @@
-class NetworkDiagram {}
+import { layoutDefault } from "./layout/layout";
+import {
+  NetworkDiagramEdgeInfo,
+  NetworkDiagramNodeInfo,
+} from "./layout/layout.type";
+import { RenderingLayer } from "./renderingLayer/renderingLayer";
+import { EventHandler, WindowLayer } from "./windowLayer/windowLayer";
+
+export class NetworkDiagram {
+  private _container: HTMLCanvasElement;
+  private _scale: number;
+  private _window?: WindowLayer;
+  private _renderingLayer?: RenderingLayer;
+
+  constructor({
+    container,
+    nodes,
+    edges,
+    eventHandler,
+  }: {
+    container: HTMLCanvasElement;
+    nodes: NetworkDiagramNodeInfo[];
+    edges: NetworkDiagramEdgeInfo[];
+    eventHandler?: Partial<EventHandler>;
+  }) {
+    this._container = container;
+    this._scale = 4;
+
+    this.init({ nodes, edges, eventHandler });
+  }
+  private async init({
+    nodes,
+    edges,
+    eventHandler,
+  }: {
+    nodes: NetworkDiagramNodeInfo[];
+    edges: NetworkDiagramEdgeInfo[];
+    eventHandler?: Partial<EventHandler>;
+  }) {
+    const layout = await layoutDefault({
+      scale: this._scale,
+      nodes,
+      edges,
+    });
+
+    this._renderingLayer = new RenderingLayer({ layout, scale: this._scale });
+    this._window = new WindowLayer({
+      renderingLayer: this._renderingLayer,
+      scale: this._scale,
+      eventHandler,
+      canvasElement: this._container,
+    });
+  }
+
+  cleanUp() {
+    this._window?.detachEvent();
+  }
+}
