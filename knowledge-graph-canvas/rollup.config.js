abd6311a52be370cadf6b1d88a8ed70935e80184
ys_park <yunseong_park@tmax.co.kr>
2024-08-19 14:30:32 +0900

    [Enhancement] 번들 크기 축소

--- knowledge-graph-canvas/rollup.config.js
+++ knowledge-graph-canvas/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.ts",
@@ -8,10 +9,12 @@ export default {
     {
       file: "dist/index.js",
       format: "cjs",
+      plugins: [terser()],
     },
     {
       file: "dist/index.esm.js",
       format: "es",
+      plugins: [terser()],
     },
   ],
   plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
